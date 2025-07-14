4cbea6af680051bc351886cc10708a08ff34e5c3
Aditya Garg <adityagarg646@gmail.com>
2025-07-14 19:28:26 +0530

    chore: Comment out Home component code for future reference

--- src/app/(Home)/page.tsx
+++ src/app/(Home)/page.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+// import React from 'react'
 
-const Home = () => {
-  return <div>page</div>
-}
+// const Home = () => {
+//   return <div>page</div>
+// }
 
-export default Home
+// export default Home
